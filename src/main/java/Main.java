bfb3cc9724100f2415651167d47f68647fbf6214
dimaakimm <dmitriykim2005@gmail.com>
2024-12-07 15:37:55 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Todos List:");
+        TodoParser.parse();
+        System.out.println("Companies list:");
+        CompaniesParser.parse();
+    }
+}
